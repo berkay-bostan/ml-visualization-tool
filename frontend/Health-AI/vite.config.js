09e3e173b307ca1cd428166a39a5f70cac0fc6c8
karakayalperen18 <c2128023@student.cankaya.edu.tr>
2026-04-18 01:12:41 +0300

    chore: setup automated GitHub Pages deployment

--- frontend/Health-AI/vite.config.js
+++ frontend/Health-AI/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ml-visualization-tool/',
   plugins: [react()],
   server: {
     proxy: {
